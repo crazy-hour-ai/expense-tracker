13faba30579d398138db7861aae6128b9bd7f042
lim <crazy.hour.ai@gmail.com>
2020-03-13 23:44:59 +0800

    Add Mongoose connection, create record & user model

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+
 const app = express();
+
 const port = 3000;
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/record', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+const db = mongoose.connection;
+
+db.on('error', () => {
+  console.log('mongodb error');
+})
+
+db.once('open', () => {
+  console.log('mongodb connected');
+})
 
 
 app.use('/', require('./routes/home.js'));
