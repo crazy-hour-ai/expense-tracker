252fd0b73903391ffd952a7baca612c636e7696b
lim <crazy.hour.ai@gmail.com>
2020-03-15 00:21:37 +0800

    Add user registration feature

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/user');
+
 router.get('/login', (req, res) => {
   res.render('login');
 })
 
 router.post('/login', (req, res) => {
+
 
 })
 
@@ -15,7 +18,34 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  res.render('register')
+  const { name, email, password, password2 } = req.body;
+
+  User.findOne({ email: email }).then(user => {
+    if (user) {
+      console.log("user exits");
+      res.render('register', {
+        name,
+        email,
+        password,
+        password2
+
+      })
+    }
+    else {
+      const newUser = new User({ // 如果 email 不存在就直接新增
+        name,
+        email,
+        password
+      })
+
+      newUser
+        .save()
+        .then(user => {
+          res.redirect('/') // 新增完成導回首頁
+        })
+        .catch(err => console.log(err))
+    }
+  })
 
 })
 
