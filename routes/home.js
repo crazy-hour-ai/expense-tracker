9ecc90dfa6fb22b31f80cbf68d37d870ed736133
lim <crazy.hour.ai@gmail.com>
2020-03-14 21:02:47 +0800

    Create category function, update search routes with query string

--- routes/home.js
+++ routes/home.js
@@ -6,14 +6,47 @@ const Record = require('../models/record.js');
 
 router.get('/', (req, res) => {
 
+  const searchKeyword = req.query.keyword;
+
+  let totalAmount = 0;
   Record.find({})
     .lean()
     .find((err, records) => {
       if (err) {
         return console.log(err);
       }
-      res.render('index', { records: records });
+      records.forEach(record => {
+        totalAmount = totalAmount + record.amount;
+      })
+
+      res.render('index', { records: records, totalAmount: totalAmount });
     })
+
+})
+
+
+router.get('/search', (req, res) => {
+
+  const searchKeyword = req.query.keyword;
+
+  let totalFilterAmount = 0;
+  Record.find({})
+    .lean()
+    .find((err, records) => {
+      if (err) {
+        return console.log(err);
+      }
+      const search = records.filter((searchResult) => {
+        return searchResult.category.includes(searchKeyword);
+      })
+      search.forEach(result => {
+        totalFilterAmount = totalFilterAmount + result.amount;
+      })
+      //search[0].amount;
+
+      res.render('index', { records: search, totalAmount: totalFilterAmount, keyword: searchKeyword });
+    })
+
 })
 
 module.exports = router;
